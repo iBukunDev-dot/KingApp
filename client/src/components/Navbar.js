1576b1b9ff4a5a414e3afc18c3cbb96f5e451815
Victor Ajagunna <82498393+iBukunDev-dot@users.noreply.github.com>
2023-06-15 20:19:24 +0000

    project setup

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+
+
+export default function Navbar() {
+    
+    return (
+        <div className='navbar'>
+            <div className='navbar-brand'>Brand</div>
+            <div className='navbar-menu'>Menu</div>
+
+        </div>
+    )
+}
